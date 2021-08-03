8ecd676e459e24fb514983fed8cd823b455d1fa8
Christopher Cohen <chris@cscohen.com>
2021-08-03 15:56:08 -0600

    got search workin working on logic for region next

--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ console.log('This is connected!')
 const recTitle = el('food-title')
 const recInstruction = el('recipe') 
 const recImg = el('food-image')
+const searchButton = el('food-search')
 const randomMealButton = el('randomFood')
 const randomRec = document.getElementById('meal');
 const mealSearch = 'http://www.themealdb.com/api/json/v1/1/search.php?s='
 const getRandomRec = 'https://themealdb.com/api/json/v1/1/random.php'
 
+const generateRegionMeal = meal => {
+        const regionalMenu = []
+        //loop throught the data returned from the randomGenerator
+        for (let i = 1; i <= 30; i++) {
+            if (meal[`strMeal${i}`]) {
+                regionalMenu.push(
+                    `${meal[`strMeal${i}`]} - ${meal[`strMeasure${i}`]}`
+                );
+            } else {
+                //no more ingredients, stop
+                break
+            }
+        }
+        //append the data from returned data and make it into something readable
+        //there has to be a better way for this one this needs formatting as well
+        const newInnerHTML = `
+            <div>
+                <div>
+                    <img src="${meal.strMealThumb}" alt="Meal Image" id='recipeImage'>
+                </div>
+                    <h2>${meal.strMeal}</h2>
+            </div>
+        `
+    //    recTitle = meal.strMeal
+    // how to get title above the button to also generate
+        randomRec.innerHTML = newInnerHTML;
+    }
 //generate the meal
 const generateMeal = meal => {
     const ingredients = []
@@ -18,7 +46,7 @@ const generateMeal = meal => {
 			);
 		} else {
             //no more ingredients, stop
-			break;
+			break
 		}
 	}
     //append the data from returned data and make it into something readable
@@ -52,17 +80,33 @@ randomMealButton.addEventListener('click', () => {
     })
 })
 
+//searchButton.addEventListener('submit',recipieSearch())
+
 //trying initial api once again
 
 //api search using a term fetches url and concats the search value
 function recipeSearch(mealName) {
-    fetch(mealSearch, + mealName)
+const mealSearchURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`+mealName
+    fetch(mealSearchURL)
     .then(resp => resp.json())
     .then(resp => {
-        generateMeal(res.meals[0])
+        generateMeal(resp.meals[0])
     })
 }
-
+function regionSearch(region) {
+    const regionSearchURL = `https://www.themealdb.com/api/json/v1/1/filter.php?a=`+region
+    fetch(regionSearchURL)
+        .then(resp => resp.json())
+        .then(resp => {
+            generateRegionMeal(resp.meals[0])
+        })
+    }
+//console.log(regionSearch('Canadian'))
+// need to build a for loop for the search if there are more than one result:
+/*
+should be able to use generate meal in recipeSearch...
+}
+*/
 
 function el(id) {
     return document.getElementById(id)
