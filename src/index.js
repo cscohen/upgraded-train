ca6fb589160aca4f477298d192c5811990e539bb
Christopher Cohen <chris@cscohen.com>
2021-08-03 09:14:44 -0600

    got random working, need to clean up css and html

--- src/index.js
+++ src/index.js
@@ -1,25 +1,68 @@
 console.log('This is connected!')
-//will concantinating like this work or does the api key need to be added after the call, will test
-//const searchRec = `https://api.spoonacular.com/food/products/search?query=${SEARCH}&apiKey=cebc02cede0d49a7826aebdc2841d080`
-//?apiKey=cebc02cede0d49a7826aebdc2841d080
-const getRandomRec = 'https://api.spoonacular.com/recipes/random?apiKey=cebc02cede0d49a7826aebdc2841d080'
+const recTitle = el('food-title')
+const recInstruction = el('recipe') 
+const recImg = el('food-image')
+const randomMealButton = el('randomFood')
+const randomRec = document.getElementById('meal');
+const mealSearch = 'http://www.themealdb.com/api/json/v1/1/search.php?s='
+const getRandomRec = 'https://themealdb.com/api/json/v1/1/random.php'
 
-fetch(getRandomRec)
-.then(resp => resp.json())
-.then(buildRecipe)
+//generate the meal
+const generateMeal = meal => {
+    const ingredients = []
+    //loop throught the data returned from the randomGenerator
+    for (let i = 1; i <= 30; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(
+				`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+			);
+		} else {
+            //no more ingredients, stop
+			break;
+		}
+	}
+    //append the data from returned data and make it into something readable
+    const newInnerHTML = `
+		<div class="row">
+			<div class="columns five">
+				<img src="${meal.strMealThumb}" alt="Meal Image" id='recipeImage'>
+			</div>
+				<h2>${meal.strMeal}</h2>
+                ${meal.strArea ? `<p><strong>Region:</strong> ${meal.strArea}</p>` : ''}
+                <h5>Ingredients:</h5>
+				<ul>
+					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+				</ul>
+				<p>${meal.strInstructions}</p>
 
-function recipeSearch() {
-    
+		</div>
+	`
+//    recTitle = meal.strMeal
+// how to get title above the button to also generate
+    randomRec.innerHTML = newInnerHTML;
+};
+
+//uses the random api fetch to grab a random recipe on click
+randomMealButton.addEventListener('click', () => {
+    fetch(getRandomRec)
+    .then(resp => resp.json())
+    .then(res => {
+        generateMeal(res.meals[0])
+    })
+})
+
+//trying initial api once again
+
+//api search using a term fetches url and concats the search value
+function recipeSearch(mealName) {
+    fetch(mealSearch, + mealName)
+    .then(resp => resp.json())
+    .then(resp => {
+        generateMeal(res.meals[0])
+    })
 }
-function randomRec() {
 
-}
-function buildRecipe(data) {
-    recTitle = data.title
-//    recImg.src = data.image
-    recAbout = data.recAbout
-    recInstruction = data.insturctions
-    recIngredients = data.extendedIngredients
-    console.log(data)
 
+function el(id) {
+    return document.getElementById(id)
 }
